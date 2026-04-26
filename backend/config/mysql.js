38be9c19285ded3ca8a522f141b0e534fe77e0c9
Nawin chander <nawinnapzy289@gmail.com>
2026-04-26 12:11:07 +0530

    add

--- backend/config/mysql.js
+++ backend/config/mysql.js
@@ -14,3 +14,13 @@ db.connect(err => {
 
 module.exports = db;
 
+
+// CREATE DATABASE vueappdb;
+// USE vueappdb;
+
+// CREATE TABLE users (
+//   id INT AUTO_INCREMENT PRIMARY KEY,
+//   name VARCHAR(100),
+//   email VARCHAR(100)
+// );
+

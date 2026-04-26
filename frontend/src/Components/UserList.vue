1c9d7dbd214ab72aa988672e6649d287aa0f15c3
Nawin chander <nawinnapzy289@gmail.com>
2026-04-26 12:09:00 +0530

    add

---
+++ frontend/src/Components/UserList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>User List</h3>
+    <ul>
+      <li v-for="user in users" :key="user._id">
+        {{ user.name }} - {{ user.email }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['users']
+}
+</script>
+

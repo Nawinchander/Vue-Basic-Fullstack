8dc412ad6a04b8fbe212a7535d2b7dbf77237dae
Nawin chander <nawinnapzy289@gmail.com>
2026-04-26 12:00:24 +0530

    add

---
+++ backend/config/mysql.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'vueappdb'
+});
+
+db.connect(err => {
+  if (err) throw err;
+  console.log('MySQL Connected');
+});
+
+module.exports = db;
+

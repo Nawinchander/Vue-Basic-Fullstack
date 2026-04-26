39f0a392d629b78bef3ce7a302662e26e207d4bf
Nawin chander <nawinnapzy289@gmail.com>
2026-04-26 12:08:33 +0530

    add

---
+++ frontend/src/Components/UserForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input v-model="name" placeholder="Enter Name" />
+    <input v-model="email" placeholder="Enter Email" />
+    <button @click="submitUser">Add User</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      name: '',
+      email: ''
+    }
+  },
+  methods: {
+    async submitUser() {
+      await axios.post('http://localhost:5000/api/users', {
+        name: this.name,
+        email: this.email
+        });
+      this.$emit('user-added');
+      this.name = '';
+      this.email = '';
+    }
+  }
+}
+</script>
+

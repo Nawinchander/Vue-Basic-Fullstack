7cc2ff7ad1c5b48d7ccdf363c9e832acfb2a2cc0
Nawin chander <nawinnapzy289@gmail.com>
2026-04-27 10:16:45 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const connectMongo = require('./config/mongo');
 const userRoutes = require('./routes/userRoutes');
+const authRoutes = require('./routes/authRoutes');
 require('./config/mysql');
 
 const app = express();
 connectMongo();
 
+
 app.use(cors());
 app.use(express.json());
 app.use('/api', userRoutes);
+app.use('/api/auth', authRoutes);
 
 app.listen(5000, () => console.log('Server running on port 5000'));
 

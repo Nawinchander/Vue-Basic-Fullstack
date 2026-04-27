3deb9590ce60a9c39a370005552d649a3529f57c
Nawin chander <nawinnapzy289@gmail.com>
2026-04-27 10:21:18 +0530

    add

--- frontend/src/Router/index.js
+++ frontend/src/Router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Login from '../views/Login.vue';
+import Login from '../views/login.vue';
 import Register from '../views/Register.vue';
 import Dashboard from '../views/Dashboard.vue';
 
